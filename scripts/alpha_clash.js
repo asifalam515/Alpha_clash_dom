0dff31e0a2f1e117f9acacf8abab8c68b2884ee3
asifalam515 <asibulalam515@gmail.com>
2024-07-18 11:23:29 +0600

    function added

--- scripts/alpha_clash.js
+++ scripts/alpha_clash.js
@@ -1,3 +1,11 @@
 function play() {
-  console.log("play start now");
+  // step 1:hide the home screen.to hide the screen add the class hidden in home section
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+  // show the play ground
+  const playGroundSection = document.getElementById("play-ground");
+  playGroundSection.classList.remove("hidden");
 }
+
+hideElementById("home-screen");
+showElementById("play-ground");
